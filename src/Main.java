a5f3236947f419093dc39a96e8e8a44c728bbdb7
Zaid Chamorro <zaid.chamorro@udla.edu.ec>
2026-04-14 08:38:44 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        SistemaFactura sis = new SistemaFactura();
+        Factura factura = null;
+        int opc;
+
+        do {
+            opc = SistemaFactura.menu();
+
+            switch (opc) {
+                case 1:
+                    factura = sis.crearFactura();
+                    break;
+                case 2:
+                    sis.imprimirFactura(factura);
+                    break;
+                case 3:
+                    sis.imprimirMonto(factura);
+                    break;
+                case 4:
+                    sis.actualizarFactura(factura);
+                    break;
+                case 5:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opcion no valida.");
+            }
+
+        } while (opc != 5);
     }
 }
